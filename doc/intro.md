82b206cddc85a9497153d8b2334eda68643346f0
Tom Howe <turtlebender@gmail.com>
2013-03-29 08:20:10 -0500

    initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nsca
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/nsca/core_test.clj
@@ -0,0 +1,7 @@
+(ns nsca.core-test
+  (:use clojure.test
+        nsca.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
